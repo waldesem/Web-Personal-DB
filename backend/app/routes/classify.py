1f67264c2c71e7fa43be30d93e1f08e33815f1f8
Vladimir Semenenko <wsemenenko@gmail.com>
2023-09-20 10:11:12 +0300

    Refactoring classes  view

--- backend/app/routes/classify.py
+++ backend/app/routes/classify.py
@@ -1,22 +1,21 @@
+from flask.views import MethodView
+
 from . import bp
 from .. import db
 from ..models.classes import Category, Conclusions, Decisions, Roles, Groups, Status
 from ..models.model import Region
 
-    
-@bp.get('/classify')
-@bp.doc(hide=True)
-def get_classes():
-    """
-    Get the classification data.
-    This function retrieves the classification data for the API. It returns a dictionary 
-    containing the values of the different status options, role options, conclusions,
-    decisions, categories, groups and roles 
-    """
-    return [{i.name: i.value for i in Status}, 
-            {rgn[0]: rgn[1] for rgn in db.session.query(Region.id, Region.region).all()}, 
-            {i.name: i.value for i in Conclusions}, 
-            {i.name: i.value for i in Decisions}, 
-            {i.name: i.value for i in Category},
-            {i.name: i.value for i in Groups},
-            {i.name: i.value for i in Roles}]
+
+class ClassesView(MethodView):
+
+    @bp.doc(hide=True)
+    def get(self):
+        return [{i.name: i.value for i in Status}, 
+                {rgn[0]: rgn[1] for rgn in db.session.query(Region.id, Region.region).all()}, 
+                {i.name: i.value for i in Conclusions}, 
+                {i.name: i.value for i in Decisions}, 
+                {i.name: i.value for i in Category},
+                {i.name: i.value for i in Groups},
+                {i.name: i.value for i in Roles}]
+
+bp.add_url_rule('/classes', view_func=ClassesView.as_view('classes'))
