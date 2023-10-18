1b5c9216c6309358fc8751ad74d2286ebe6241ce
Vladimir Semenenko <wsemenenko@gmail.com>
2023-10-18 13:26:48 +0300

    Fix analysis changes

--- backend/app/utils/analysis.py
+++ backend/app/utils/analysis.py
@@ -1,9 +1,12 @@
 # pip install spacy
 # pip install ru-core-news-sm
+# pip install natasha
 
+import re
 import string
 from collections import Counter
 
+# import natasha
 import spacy
 from spacy.lang.ru.stop_words import STOP_WORDS
 
@@ -11,23 +14,26 @@ nlp = spacy.load("ru_core_news_sm")
 
 def analyse_text(data: dict):
 
-    data = {k: str(v) for k, v in data.items}
-
-    lst_origin = list(data.values())
+    list_data = [str(v) for _, v in data.items]
     
-    lst = [item.lower() for item in lst_origin if not item.isdigit() and not item.isalpha()]
-    lst = [item for item in lst if item not in STOP_WORDS]
-    text = ' '.join(list(lst))
-    txt = text.replace('«', '').replace('»', '').replace('—', '').replace('-', '')
+    lst = [item.lower() for item in list_data \
+           if item.lower() not in STOP_WORDS and not item.isdigit()]
+
+    text = ' '.join(list(lst)).strip()
+    txt = text.replace('«', '').replace('»', '')
+    txt = re.sub('\s+', ' ', txt)
     txt = txt.translate(str.maketrans('', '', string.punctuation))
 
     doc = nlp(txt)
-    lemmas = [token.lemma_ for token in doc if token.pos_ == 'NOUN' or token.pos_ == 'VERB']
+
+    lemmas = [token.lemma_ for token in doc]
     word_frequency = Counter(lemmas)
     frequency = {word for word, freq in word_frequency.items() if freq > 1}
-    
-    text_2 = ' '.join(list(lst_origin))
-    doc1 = nlp(text_2)
-    named = {ent.text for ent in doc1.ents}
 
-    return frequency.update(named)
+    named = {token for token in doc.ents}
+
+    return frequency.union(named)
+
+if __name__ == '__main__':
+    dct = {}
+    analyse_text(dct)
