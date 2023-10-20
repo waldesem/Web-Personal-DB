642af32b583c4cee61614cfcc219a03f3da5f6da
Vladimir Semenenko <wsemenenko@gmail.com>
2023-10-20 20:26:40 +0300

    Fix issues, some updates

--- backend/app/utils/analysis.py
+++ backend/app/utils/analysis.py
@@ -2,7 +2,6 @@ import re
 import string
 from collections import Counter
 
-# import natasha
 import spacy
 from spacy.lang.ru.stop_words import STOP_WORDS
 
@@ -11,15 +10,17 @@ from faker import Faker
 
 fake = Faker("ru-RU")
 
-nlp = spacy.load("ru_core_news_sm")
+nlp = spacy.load("ru_core_news_md")
 
 def analyse_text(data: dict):
 
-    list_data = [str(v) for k, v in data.items() if k not in ['id', 'request_id', 'person_id']]
+    list_data = [str(v) for k, v in data.items() \
+                 if k not in ['id', 'request_id', 'person_id']]
     
     digital = {digit for digit in list_data if digit.isdigit()}
 
-    dates = {d for d in list_data if re.match(r'\d\d.\d\d.\d\d\d\d', d) or re.match(r'\d\d\d\d-\d\d-\d\d', d)}
+    dates = {d for d in list_data if re.match(r'\d\d.\d\d.\d\d\d\d', d) \
+             or re.match(r'\d\d\d\d-\d\d-\d\d', d)}
 
     lst = [item.lower() for item in list_data \
            if item.lower() not in STOP_WORDS]
@@ -32,11 +33,13 @@ def analyse_text(data: dict):
     doc = nlp(txt)
 
     lemmas = [token.lemma_ for token in doc \
-              if token.pos_ == 'NOUN' or token.pos_ == 'VERB']
+              if token.pos_ == 'NOUN' or token.pos_ == 'VERB' \
+                or token.pos_ == 'ADJ' or token.pos_ == 'ADV']
+    
     word_frequency = Counter(lemmas)
     frequency = {word for word, freq in word_frequency.items() if freq > 1}
 
-    named = {token for token in doc.ents}
+    named = {token.text for token in doc.ents}
 
     return frequency.union(named).union(digital).union(dates)
 
@@ -68,6 +71,9 @@ if __name__ == '__main__':
         'mvd': fake.text(max_nb_chars=100),
         'path': ''
     }
+    finall = set()
     for s in [resume, check]:
         tag_set = analyse_text(s)
-        print(tag_set)
+        finall = finall.union(tag_set)
+    
+    print(' '.join(finall))
