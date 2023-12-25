6d653c761b157ae3e239e02b001311e9c27a4647
Vladimir Semenenko <wsemenenko@gmail.com>
2023-12-25 10:57:30 +0300

    Update cli

--- backend/cli.py
+++ backend/cli.py
@@ -1,15 +1,13 @@
+from datetime import datetime
 import os
 import bcrypt
 
-from config import basedir
 from app import db
 from config import Config
-from app.models.model import User, Region, Role, Group, \
+from app.models.model import Person, User, Region, Role, Group, \
     Status, Conclusion, Category
 from app.models.classes import Roles, Groups, Regions, \
     Statuses, Conclusions, Categories
-from app.utils.jsonparser import JsonFile
-from app.routes.route import ResumeView, FileView
 
 
 def register_cli(app):
@@ -40,22 +38,34 @@ def register_cli(app):
         db.session.flush()
 
         superadmin = User(
-            fullname='Superadmin',
             username='superadmin',
             password=bcrypt.hashpw(Config.DEFAULT_PASSWORD.encode('utf-8'), 
                                    bcrypt.gensalt()))
-        
         db.session.add(superadmin)
         db.session.flush()
-
+        
         superadmin.roles.append(Role().get_role(Roles.admin.value))
         superadmin.groups.append(Group().get_group(Groups.admins.value))
         db.session.add(superadmin)
 
-        db.session.commit()
+        db.session.add(Person(
+            category_id = Category().get_id(Categories.candidate.value),
+            region_id = Region().get_id(Regions.MAIN_OFFICE.value),
+            fullname = 'Бендер Остап Сулеман',
+            previous = 'Ильф и Петров',
+            birthday = datetime.now().date() - 100,
+            birthplace = 'г.Нью-Васюки',
+            country = 'Россия',
+            ext_country = 'Турция',
+            snils = '12345678901',
+            inn = '123456789012',
+            education = 'Университет Джордано Бруно',
+            marital = 'не женат',
+            addition = 'Холодный философ и свободный художник',
+            path = Config.BASE_PATH,
+            status_id = Status().get_id(Statuses.new.value)
+        ))
 
-        person = JsonFile(os.path.join(basedir, 'test.json'))
-        person_id = ResumeView().add_resume(person.resume, 'create')
-        FileView().fill_items(person, person_id)
+        db.session.commit()
         
         print('Models created and filled')
