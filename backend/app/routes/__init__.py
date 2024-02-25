df3f5b82fa60917583f6741a85027a4cc4ce15b8
Vladimir Semenenko <wsemenenko@gmail.com>
2024-02-25 09:43:40 +0300

    Refactoring code

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -3,13 +3,9 @@ from apiflask import APIBlueprint
 bp = APIBlueprint("route", __name__)
 
 from . import admin
-from . import anketa
-from . import checks
+from . import candidate
 from . import classify
 from . import contact
-from . import files
-from . import index
 from . import login
 from . import manager
 from . import message
-from . import resume
