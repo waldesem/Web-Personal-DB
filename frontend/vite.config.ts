953d6a102e40612419364aea31c8733c1b4fa44f
Vladimir Semenenko <wsemenenko@gmail.com>
2024-04-20 07:28:51 +0300

    Fix aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,18 +11,6 @@ export default defineConfig({
         replacement: fileURLToPath(new URL("./src", import.meta.url)),
       },
       {
-        find: "@store",
-        replacement: fileURLToPath(new URL("./src/store", import.meta.url)),
-      },
-      {
-        find: "@utilities",
-        replacement: fileURLToPath(new URL("./src/utilities", import.meta.url)),
-      },
-      {
-        find: "@router",
-        replacement: fileURLToPath(new URL("./src/router", import.meta.url)),
-      },
-      {
         find: "@components",
         replacement: fileURLToPath(
           new URL("./src/components", import.meta.url)
