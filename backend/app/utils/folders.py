3b89fc0fcfd6d87a2eda976e29567d69d8a3d717
Vladimir Semenenko <wsemenenko@gmail.com>
2024-04-27 20:50:13 +0300

    Update class.

--- backend/app/utils/folders.py
+++ backend/app/utils/folders.py
@@ -10,24 +10,30 @@ class Folders:
         self.url = os.path.join(
             Config.BASE_PATH,
             surname[0].upper(),
-            f"{person_id}-{surname.upper()} {firstname.upper()} {patronymic.upper()}".rstrip(),
+            f"{person_id}-{surname.upper()} "
+            f"{firstname.upper()} "
+            f"{patronymic.upper()}".rstrip(),
         )
 
+    @classmethod
+    def _check_url(cls, url):
+        if not os.path.isdir(url):
+            os.mkdir(url)
+
     def create_main_folder(self):
-        if not os.path.isdir(self.url):
-            os.mkdir(self.url)
+        self._check_url(self.url)
         return self.url
 
     def create_parent_folder(self, folder_name):
-        parent_folder = os.path.join(self.url, folder_name)
-        if not os.path.isdir(parent_folder):
-            os.mkdir(parent_folder)
+        parent_folder = os.path.join(self.create_main_folder(), folder_name)
+        self._check_url(parent_folder)
         return parent_folder
 
     def create_subfolder(self, parent_folder):
         subfolder = os.path.join(
-            self.url, parent_folder, datetime.now().strftime("%Y-%m-%d")
+            self.url,
+            self.create_parent_folder(parent_folder),
+            datetime.now().strftime("%Y-%m-%d"),
         )
-        if not os.path.isdir(subfolder):
-            os.mkdir(subfolder)
+        self._check_url(subfolder)
         return subfolder
