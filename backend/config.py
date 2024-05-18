6507602c64c9ac7154f61c554155f2c737fde335
Vladimir Semenenko <wsemenenko@gmail.com>
2024-05-18 20:23:54 +0300

    Update config

--- backend/config.py
+++ backend/config.py
@@ -1,22 +1,21 @@
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Settings(BaseSettings):
-    jwt_secret_key: str | None = os.getenv("jwt_secret_key")
+    jwt_secret_key: str | None
     jwt_access_token_expires: timedelta = timedelta(seconds=600)
     jwt_refresh_token_expires: timedelta = timedelta(days=30)
     jwt_tokens_algorithm: str = "HS256"
-    sqlalchemy_database_uri: str | None = os.getenv("sqlalchemy_database_uri")
+    sqlalchemy_database_uri: str | None
     base_path: str = os.path.abspath(os.path.join(basedir, "..", "persons"))
     default_password: str = "8" * 8
     pagination: int = 16
 
+    model_config = SettingsConfigDict(env_file = os.path.join(basedir, ".env"))
 
 settings = Settings()
