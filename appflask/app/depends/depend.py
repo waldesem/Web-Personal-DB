cc745d5b027ab34b26ec19e25741daa1ef0f7dd8
Vladimir Semenenko <wsemenenko@gmail.com>
2024-08-15 15:49:24 +0300

    Add experimental app

---
+++ appflask/app/depends/depend.py
@@ -0,0 +1,122 @@
+from base64 import b64decode, b64encode
+from datetime import datetime
+from functools import lru_cache, wraps
+
+from flask import abort, current_app, g, request
+from werkzeug.local import LocalProxy
+
+from ..model.tables import Users, db_session
+
+current_user = LocalProxy(lambda: get_current_user(g.user_id))
+
+
+def get_auth(token):
+    """
+    Decode the given token and check if it matches the secret key.
+
+    Args:
+        token (str): The token to be decoded.
+
+    Returns:
+        bool: True if the decoded token matches the secret key, False otherwise.
+    """
+    try:
+        decoded = b64decode(token.split(" ", 1)[1]).decode().split(":", 1)
+        secret_key, g.user_id = decoded
+        return secret_key == current_app.config["SECRET_KEY"]
+    except (IndexError, UnicodeDecodeError):
+        return False
+
+
+@lru_cache(maxsize=8)
+def get_current_user(user_id):
+    """
+    Retrieve the current user based on the user ID stored in the global variable 'g.user_id'.
+
+    Args:
+        user_id (int): The ID of the user.
+
+    Returns:
+        dict or None: A instance containing the user's information if the user exists,
+            is not blocked, not deleted, and has not changed password in the last year.
+            Otherwise, returns None.
+    """
+    user = db_session.get(Users, user_id)
+    delta_change = datetime.now() - user.pswd_create
+    if (
+        user
+        and not user.blocked
+        and not user.deleted
+        and not user.change_pswd
+        and delta_change.days < 365
+    ):
+        return user.to_dict()
+    return None
+
+
+def create_token(user):
+    """
+    Generate a token for the given user.
+
+    Args:
+        user (dict): A dictionary containing the user's information.
+
+    Returns:
+        str: The generated token, encoded in base64.
+
+    This function takes a dictionary containing the user's information
+    and generates a token using the secret key and the user's id.
+    The token is then encoded in base64 and returned as a string.
+    """
+    token_parts = [
+        current_app.config["SECRET_KEY"],
+        str(user.id),
+    ]
+    token = ":".join(token_parts)
+    return b64encode(token.encode()).decode()
+
+
+def jwt_required():
+    """
+    Decorator function that checks if the request contains a valid JWT token.
+
+    The decorated function checks if the request contains a valid JWT token in the
+    'Authorization' header. If the token is valid, the decorated function is executed.
+    Otherwise, a 401 HTTP status code is returned.
+
+    Parameters:
+        func (function): The function to be decorated.
+
+    Returns:
+        function: The decorated function.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            header = request.headers.get("Authorization")
+            if header and get_auth(header):
+                return func(*args, **kwargs)
+            abort(401)
+
+        return wrapper
+
+    return decorator
+
+
+def roles_required(*roles):
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            header = request.headers.get("Authorization")
+            if header and get_auth(header):
+                cur_user = current_user
+                if cur_user and cur_user['role'] in roles:
+                    return func(*args, **kwargs)
+                abort(403)
+            abort(401)
+
+        return wrapper
+
+    return decorator
---
+++ appflask/server.py
@@ -0,0 +1,59 @@
+import argparse
+
+from app import create_app
+from waitress import serve
+from webgui import FlaskUI
+
+
+def main():
+    """
+    A function that runs the application server based on the provided arguments.
+    You also need to check config.py for configuration class.
+    Example usage:
+        For debugging:
+            python server.py --host 127.0.0.1 --port 5000 --mode debug
+
+        For development:
+            python server.py --host 127.0.0.1 --port 5000 --mode develop
+
+        For production:
+            python server.py --host 127.0.0.1 --port 5000 --mode prod
+
+        For desktop:
+            python server.py
+
+    """
+    parser = argparse.ArgumentParser(description="Run the application server.")
+    parser.add_argument(
+        "--host", default="127.0.0.1", help="The host to bind the server to."
+    )
+    parser.add_argument(
+        "--port", default=5000, type=int, help="The port to run the server on."
+    )
+    parser.add_argument(
+        "--threads", default=8, type=int, help="The number of threads to use."
+    )
+    parser.add_argument(
+        "--mode",
+        choices=["debug", "develop", "prod", "desktop"],
+        default="desktop",
+        help="The mode to run the server in (debug, develop, prod, desktop).",
+    )
+    args = parser.parse_args()
+
+    app = create_app()
+
+    if args.mode == "debug":
+        app.run(host=args.host, port=args.port, debug=True)
+    elif args.mode == "develop":
+        app.run(host=args.host, port=args.port, debug=False)
+    elif args.mode == "prod":
+        serve(app, host=args.host, port=args.port, threads=args.threads)
+    elif args.mode == "desktop":
+        FlaskUI(
+            server_kwargs={"app": app, "port": args.port, "threads": args.threads},
+        ).run()
+
+
+if __name__ == "__main__":
+    main()
