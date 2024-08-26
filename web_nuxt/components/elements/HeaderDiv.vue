afa0c60f3aabd6c1471a3d6f34cc0b3005e33379
waldesem <105578047+waldesem@users.noreply.github.com>
2024-08-26 14:24:34 +0300

    Update elements

---
+++ web_nuxt/components/elements/HeaderDiv.vue
@@ -0,0 +1,28 @@
+ <script setup lang="ts">
+	const props = defineProps({
+		div: {
+			type: String,
+			default: 'py-8'
+		},
+		cls: {
+			type: String,
+			default: 'text-2xl text-red-800'
+		},
+		header: {
+			type: String,
+			default: ""
+		}
+	}
+	</script>
+	
+	<template>
+		<div :class="props.div">
+		  <h3 
+			class="font-bold"
+			:class="props.cls"
+		  >
+			{{ props.header }}
+		  </h3>
+		</div>
+	</template>
+	
