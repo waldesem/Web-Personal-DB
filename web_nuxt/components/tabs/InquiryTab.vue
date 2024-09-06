0cbe6aaf51728d68de45fc67fe551e5372258231
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-06 07:07:26 +0300

    Update InquiryTab.vue

--- web_nuxt/components/tabs/InquiryTab.vue
+++ web_nuxt/components/tabs/InquiryTab.vue
@@ -4,6 +4,8 @@ import type { Needs } from "@/utils/interfaces";
 
 const anketaState = stateAnketa();
 
+await anketaState.getItem('inquiries);
+
 const collapse = ref(false);
 const edit = ref(false);
 const itemId = ref("");
