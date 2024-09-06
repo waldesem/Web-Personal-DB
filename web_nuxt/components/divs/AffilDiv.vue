36b05fb6312a8cb00acdfc9e6311c84e455a3d24
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-06 07:09:21 +0300

    Update AffilDiv.vue

--- web_nuxt/components/divs/AffilDiv.vue
+++ web_nuxt/components/divs/AffilDiv.vue
@@ -4,6 +4,8 @@ import type { Affilation } from "@/utils/interfaces";
 
 const anketaState = stateAnketa();
 
+await anketaState.getItem('affilations);
+
 const collapse = ref(false);
 const edit = ref(false);
 const itemId = ref("");
