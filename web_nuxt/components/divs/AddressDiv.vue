57470be93935ec47b2f620babd307eabf57530fa
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-06 07:10:59 +0300

    Update AddressDiv.vue

--- web_nuxt/components/divs/AddressDiv.vue
+++ web_nuxt/components/divs/AddressDiv.vue
@@ -4,7 +4,7 @@ import type { Address } from "@/utils/interfaces";
 
 const anketaState = stateAnketa();
 
-await anketaState.getItem('addresses);
+await anketaState.getItem('addresses');
 
 const collapse = ref(false);
 const edit = ref(false);
