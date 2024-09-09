3e58776319f3bb9b5242cd899a6c96b4f1af72bd
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-09 16:30:05 +0300

    Update LabelSlot.vue

--- web_nuxt/components/elements/LabelSlot.vue
+++ web_nuxt/components/elements/LabelSlot.vue
@@ -6,17 +6,17 @@ const props = defineProps({
   },
   labelClass: {
     type: String,
-    default: "col-span-2",
+    default: "col-span-1",
   },
   inputClass: {
     type: String,
-    default: "col-span-4 break-words",
+    default: "col-span-5 break-words",
   }
 });
 </script>
 
 <template>
-  <div class="flex grid grid-cols-6 gap-3 mb-3">
+  <div class="flex grid grid-cols-6 gap-3 mb-3 text-sm text-gray-500 dark:text-gray-400">
     <div :class="props.labelClass">
       {{ props.label }}
     </div>
