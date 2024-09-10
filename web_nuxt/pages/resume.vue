55bb19f9e7360e13cbefcde6adeff696c6d40bb7
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-10 07:46:28 +0300

    Update resume.vue

--- web_nuxt/pages/resume.vue
+++ web_nuxt/pages/resume.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
+import { useFetchAuth } from "@/utils/auth";
+import { server, stateAnketa } from "@/state/state";
+import type { Persons } from "@/utils/interfaces";
+
+const authFetch = useFetchAuth();
+
+const anketaState = stateAnketa();
+
+const toast = useToast();
 
 const switchToPersons = async() => {
   navigateTo('/persons');
 };
+
+async function submitResume(): Promise<void> {
+    const response = await authFetch(`${server}/resume`, {
+      method: "POST",
+      body: resumeForm.value,
+    });
+    console.log(response);
+    toast.add({
+      icon: "i-heroicons-check-circle",
+      title: "Информация",
+      description: `Данные успешно добавлены`,
+      color: "green",
+    });
+  }
+  switchToPersons();
 </script>
 
 <template>
   <LayoutsMenu>
     <ElementsHeaderDiv :div="'mb-6'" :header="'НОВАЯ АНКЕТА'"/>
     <UCard>
-      <FormsResumeForm :action="'create'" @cancel="switchToPersons"/>
+      <FormsResumeForm 
+        @cancel="switchToPersons"
+        @update="/>
     </UCard>
   </LayoutsMenu>
 </template>
