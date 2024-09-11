8032ec84f6132639c49819ba0c44184e8edfa420
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-11 18:35:46 +0300

    Update PoligrafTab.vue

--- web_nuxt/components/tabs/PoligrafTab.vue
+++ web_nuxt/components/tabs/PoligrafTab.vue
@@ -69,7 +69,7 @@ function openFileForm(elementId: string) {
     <div
       v-for="(item, index) in anketaState.anketa.value.poligrafs"
       :key="index"
-      class="py-1"
+      class="text-sm text-gray-500 dark:text-gray-400 py-1"
     >
       <UCard>
         <template #header>
