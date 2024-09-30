ffd2ceb2bae7868d4f2483308069780ba42f4401
waldesem <105578047+waldesem@users.noreply.github.com>
2024-09-30 18:41:26 +0300

    Update handler.py

--- server_flask/app/handlers/handler.py
+++ server_flask/app/handlers/handler.py
@@ -61,7 +61,7 @@ def handle_post_item(json_data, item, item_id=None):
         json_dict = model(**json_data).dict()
         if item != "persons":
             json_dict["person_id"] = item_id
-        json_dict["user_id"] = current_user.get("id")
+            json_dict["user_id"] = current_user.get("id")
         db_session.merge(table(**json_dict))
         db_session.commit()
     except ValidationError:
