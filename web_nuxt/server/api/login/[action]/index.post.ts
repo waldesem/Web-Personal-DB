f2ee2c023beadd027ac5767a662c9da1623e3dad
waldesem <105578047+waldesem@users.noreply.github.com>
2024-10-07 18:59:10 +0300

    Update index.post.ts

--- web_nuxt/server/api/login/[action]/index.post.ts
+++ web_nuxt/server/api/login/[action]/index.post.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async(event) => {
   const action = getQuery(event).action
-  const json_data = await readBody(event)
+  const json_data = await readJSONBody(event)
   return { data: json_data, action: action }
 })
