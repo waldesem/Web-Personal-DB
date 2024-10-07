e657e1028e37ab2f65cceb07a55ccaa778d01eab
waldesem <105578047+waldesem@users.noreply.github.com>
2024-10-07 18:58:45 +0300

    Update index.post.ts

--- web_nuxt/server/api/item/[item]/[item_id]/index.post.ts
+++ web_nuxt/server/api/item/[item]/[item_id]/index.post.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async(event) => {
   const item = getRouterParam(event, 'item')
   const item_id = getRouterParam(event, 'item_id')
-  const data = await readBody(event)
+  const data = await readJSONBody(event)
   return { item: item, item_id: item_id, data: data }
 })
