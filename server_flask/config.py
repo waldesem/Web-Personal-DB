39526f1a2eb9d2ef56fa67c9b751a76ab20bd1e6
waldesem <105578047+waldesem@users.noreply.github.com>
2024-10-11 16:18:00 +0300

    Update config.py

--- server_flask/config.py
+++ server_flask/config.py
@@ -12,8 +12,7 @@ class Configuration:
     SECRET_KEY = secrets.token_hex(16)
     JWT_SECRET_KEY = secrets.token_hex(16)
     BASE_PATH = setting["Destination"].get("path")
-    DATABASE_URI = "".join(["sqlite:///", os.path.join(
-        setting["SQLite"].get("uri"), "database.db"
+    DATABASE_URI = "".join(["sqlite:///", BASE_PATH, "database.db"
     )])
     DEFAULT_PASSWORD = "8" * 8
 
