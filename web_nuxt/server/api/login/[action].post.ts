3c376e1ee49c4df98e49fd7d9bf9ba60890a8b95
Vladimir Semenenko <wsemenenko@gmail.com>
2024-10-13 19:11:26 +0300

    Update server

--- web_nuxt/server/api/login/[action].post.ts
+++ web_nuxt/server/api/login/[action].post.ts
@@ -1,5 +1,49 @@
-export default defineEventHandler(async(event) => {
-  const action = getQuery(event).action
-  const json_data = await readBody(event)
-  return { data: json_data, action: action }
-})
+import { drizzle } from "db0/integrations/drizzle";
+import { like } from "drizzle-orm";
+import { db } from "~/server/db/index";
+import { usersTable } from "~/server/db/src/schema";
+import {
+  JWT_SECRET_KEY,
+  checkPasswordHash,
+  createPasswordHash,
+  createJwtToken,
+} from "~/server/utils";
+
+export default defineEventHandler(async (event) => {
+  const action = getQuery(event).action;
+  const json_data = await readBody(event);
+  const drizzleDb = drizzle(db);
+  const query = drizzleDb
+    .select()
+    .from(usersTable)
+    .where(like(usersTable.username, `%${json_data["username"]}%`));
+  const results = await query.all();
+  const user = results[0];
+  if (!user || user.blocked || user.deleted) {
+    return { message: "Invalid" };
+  }
+  if (!checkPasswordHash(json_data["password"], user.passhash)) {
+    if (user.attempt < 5) {
+      user.attempt += 1;
+    } else {
+      user.blocked = true;
+      return { message: "Invalid" };
+    }
+  }
+  if (action == "update") {
+    user.attempt = 0;
+    user.change_pswd = false;
+    user.passhash = createPasswordHash(json_data["password"]);
+    return { message: "Updated" };
+  }
+
+  const delta = new Date().getTime() - user.pswd_create.getTime();
+  if (delta > 86400000) {
+    user.attempt = 0;
+    const token = createJwtToken(user, JWT_SECRET_KEY);
+    if (token) {
+      return { message: "Success", user_token: token };
+    }
+  }
+  return { message: "Denied" };
+});
---
+++ web_nuxt/server/utils/index.ts
@@ -0,0 +1,43 @@
+import crypto from "node:crypto";
+import jwt from "jsonwebtoken";
+
+export const JWT_SECRET_KEY = crypto.randomBytes(16).toString("hex");
+/**
+ * Creates a password hash using the crypto module.
+ * @param {string} password - The password to hash.
+ * @returns {string} The hashed password.
+ */
+export function createPasswordHash(password: string): string {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hash = crypto.createHmac("sha256", salt);
+  hash.update(password);
+  return `${salt}:${hash.digest("hex")}`;
+}
+
+/**
+ * Checks a password against a stored hash.
+ * @param {string} password - The password to check.
+ * @param {string} hash - The stored hash.
+ * @returns {boolean} True if the password matches the hash, false otherwise.
+ */
+export function checkPasswordHash(password: string, hash: string): boolean {
+  const [salt, storedHash] = hash.split(":");
+  const newHash = crypto.createHmac("sha256", salt);
+  newHash.update(password);
+  return newHash.digest("hex") === storedHash;
+}
+
+/**
+ * Creates a JWT token with the given payload and secret.
+ * @param {Object} payload - The payload to include in the token.
+ * @param {string} secret - The secret key to sign the token with.
+ * @param {Object} [options] - Additional options for the token.
+ * @returns {string} The JWT token.
+ */
+export function createJwtToken(
+  payload: object,
+  secret: string,
+  options: object = {}
+): string {
+  return jwt.sign(payload, secret, options);
+}
