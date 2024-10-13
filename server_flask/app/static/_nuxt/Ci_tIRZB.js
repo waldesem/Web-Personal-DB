6068f68a87e338ce398b7605cb4cb53bba5364f4
Vladimir Semenenko <wsemenenko@gmail.com>
2024-10-13 15:53:10 +0300

    Update builds

---
+++ server_flask/app/static/_nuxt/Ci_tIRZB.js
@@ -0,0 +1,1 @@
+import{aS as e}from"./DSZgY0GN.js";const r=e("userToken","",localStorage,{mergeDefaults:!0});export{r as u};
--- server_flask/app/static/_nuxt/CsR9_4BF.js
+++
@@ -1,1 +0,0 @@
-import{aT as e}from"./lDZheyb0.js";const r=e("userToken","",localStorage,{mergeDefaults:!0});export{r as u};
