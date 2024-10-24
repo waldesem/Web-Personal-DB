69ede13b0504e902a5c529b8ff520c8ee2263081
Vladimir Semenenko <wsemenenko@gmail.com>
2024-10-24 13:59:44 +0300

    Remove unused

--- web_nuxt/server/api/logout/index.get.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const session = await useSession(event, {
-    password: SECRET_KEY,
-  });
-  await session.clear();
-  return { message: "success" };
-});
