0495dad683fc551c0abfe2a65459093651776d26
Vladimir Semenenko <wsemenenko@gmail.com>
2024-10-24 12:25:09 +0300

    Fix issues

--- web_nuxt/composables/useUserState.ts
+++ web_nuxt/composables/useUserState.ts
@@ -3,15 +3,18 @@ import type { User } from "@/types/interfaces";
 
 export const useUserState = () => {
   if (userToken.value) {
-    const payload = ref(userToken.value.split(" ")[1]);
-    const user = useState(
-      "user",
-      () =>
-        JSON.parse(
-          Buffer.from(payload.value.split(".")[1], "base64").toString()
-        ) as User
-    );
-    return user as Ref<User>;
+    const cridentials = userToken.value.split(" ");
+    if (cridentials.length > 1) {
+      const payloads = cridentials[1].split(".");
+      if (payloads.length > 1) {
+        const user = useState(
+          "user",
+          () =>
+            JSON.parse(Buffer.from(payloads[1], "base64").toString()) as User
+        );
+        if (user.value) return user as Ref<User>;
+      }
+    }
   }
   return {} as Ref<User>;
 };
