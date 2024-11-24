f7e6276f4b4a55dfe55156ceb1316c3e0c798377
Vladimir Semenenko <wsemenenko@gmail.com>
2024-11-24 17:34:28 +0300

    Update build

---
+++ server_flask/app/static/_nuxt/D17obsrK.js
@@ -0,0 +1,1 @@
+import{u as t}from"./DqFS7qGT.js";import{n as o}from"./sWx5Anc3.js";const c=()=>async(n,e={})=>{if(t.value)e.headers={...e.headers,Authorization:`${t.value}`};else return o("/login");try{return await $fetch(n,e)}catch(r){return console.error(r),o("/login")}};export{c as u};
---
+++ server_flask/app/static/_nuxt/DqFS7qGT.js
@@ -0,0 +1,1 @@
+import{aF as e}from"./sWx5Anc3.js";const r=e("userToken","",localStorage,{mergeDefaults:!0});export{r as u};
