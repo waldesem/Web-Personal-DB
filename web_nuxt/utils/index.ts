e515fa55b11648747e55d7f6173ab787372e904f
waldesem <105578047+waldesem@users.noreply.github.com>
2024-12-02 07:08:33 +0300

    Update index.ts

--- web_nuxt/utils/index.ts
+++ web_nuxt/utils/index.ts
@@ -7,7 +7,7 @@ export function getPayload(token: string | null = accessToken.value) {
     if (cridentials.length > 1) {
       const payloads = cridentials[1].split(".");
       if (payloads.length > 1) {
-        userState.value = JSON.parse(
+        stateUser.value = JSON.parse(
           Buffer.from(payloads[1], "base64").toString()
         ) as Token;
       }
