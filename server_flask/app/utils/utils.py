1f3d75262c6e56b8ace66a5237cba493d8a4d847
waldesem <105578047+waldesem@users.noreply.github.com>
2024-12-24 14:51:07 +0300

    Update utils.py

--- server_flask/app/utils/utils.py
+++ server_flask/app/utils/utils.py
@@ -1,16 +1,25 @@
 """Utils module."""
 
-import json
 import re
 from pathlib import Path
 
 from flask import current_app
-from pydantic import ValidationError
 from sqlalchemy import select
 
 from app.depends.depend import current_user
 from app.model.models import AnketaSchemaJson
-from app.model.tables import Persons, db_session
+from app.model.tables import (
+    Addresses,
+    Affilations,
+    Contacts,
+    Documents,
+    Educations,
+    Persons,
+    Previous,
+    Staffs,
+    Workplaces,
+    db_session,
+)
 
 
 def upload_resume(resume: dict) -> int:
@@ -63,128 +72,122 @@ def upload_resume(resume: dict) -> int:
     return person.id
 
 
-def json_to_dict(file_data: str) -> dict:
-    """Transform a JSON-dictionary into a python-dictionary.
-
-    :param file_data: A JSON-dictionary
-    :return: A python-dictionary.
-    """
-    json_data = json.load(file_data)
-    try:
-        anketa = AnketaSchemaJson(**json_data)
-        return {
-            "resume": {
-                "surname": anketa.last_name,
-                "firstname": anketa.first_name,
-                "patronymic": anketa.mid_name,
-                "birthday": anketa.birthday,
-                "birthplace": anketa.birthplace,
-                "citizenship": anketa.citizen,
-                "dual": anketa.additional,
-                "marital": anketa.marital_status,
-                "inn": anketa.inn,
-                "snils": anketa.snils,
-            },
-            "staffs": [
-                {
-                    "position": anketa.position_name,
-                    "department": anketa.department,
-                },
-            ],
-            "documents": [
-                {
-                    "view": "Паспорт",
-                    "digits": anketa.passport_number,
-                    "series": anketa.passport_serial,
-                    "issue": anketa.passport_issue,
-                    "agency": anketa.passport_issued,
-                },
-            ],
-            "addresses": [
-                {
-                    "view": "Адрес проживания",
-                    "addresses": anketa.valid_address,
-                },
-                {
-                    "view": "Адрес регистрации",
-                    "addresses": anketa.reg_address,
-                },
-            ],
-            "contacts": [
-                {"view": "Телефон", "contact": anketa.contact_phone},
-                {"view": "Электронная почта", "contact": anketa.email},
-            ],
-            "educations": [
-                {
-                    "view": edu.education_type,
-                    "institution": edu.institution_name,
-                    "finished": edu.end_year,
-                    "specialty": edu.specialty,
-                }
-                for edu in anketa.education
-                if anketa.education
-            ],
-            "workplaces": [
-                {
-                    "starts": work.begin_date,
-                    "finished": work.end_date,
-                    "now_work": work.current_job,
-                    "workplace": work.name,
-                    "addresses": work.address,
-                    "reason": work.fire_reason,
-                    "position": work.position,
-                }
-                for work in anketa.experience
-                if anketa.experience
-            ],
-            "previous": [
-                {
-                    "firstname": prev.first_name,
-                    "surname": prev.last_name,
-                    "patronymic": prev.mid_name,
-                    "changed": prev.year_change,
-                    "reason": prev.reason,
-                }
-                for prev in anketa.name_was_changed
-                if anketa.name_was_changed
-            ],
-            "affilations": (
-                [
-                    {
-                        "view": "Участвует в деятельности коммерческих организаций",
-                        "organization": aff.name,
-                        "inn": aff.inn,
-                    }
-                    for aff in anketa.organizations
-                    if anketa.organizations
-                ]
-                + [
-                    {
-                        "view": "Являлся государственным должностным лицом",
-                        "organization": aff.name,
-                    }
-                    for aff in anketa.state_organizations
-                    if anketa.state_organizations
-                ]
-                + [
-                    {
-                        "view": "Связанные лица работают в государственных \
-                            организациях",
-                        "organization": aff.name,
-                    }
-                    for aff in anketa.related_organizations
-                    if anketa.related_organizations
-                ]
-                + [
-                    {
-                        "view": "Являлся государственным или муниципальным служащим",
-                        "organization": aff.name,
-                    }
-                    for aff in anketa.public_organizations
-                    if anketa.public_organizations
-                ]
-            ),
-        }
-    except ValidationError:
-        current_app.logger.exception("Validation error")
-        return {}
+def get_anketa_items(anketa: AnketaSchemaJson, person_id: int) -> list:  # noqa: D103
+    items = [
+        Staffs(
+            position=anketa.position_name,
+            department=anketa.department,
+            person_id=person_id,
+            user_id=current_user.id,
+        ),
+        Documents(
+            view="Паспорт",
+            digits=anketa.passport_number,
+            series=anketa.passport_serial,
+            issue=anketa.passport_issue,
+            agency=anketa.passport_issued,
+            person_id=person_id,
+            user_id=current_user.id,
+        ),
+        Addresses(
+            view="Адрес проживания",
+            addresses=anketa.valid_address,
+            person_id=person_id,
+            user_id=current_user.id,
+        ),
+        Addresses(
+            view="Адрес регистрации",
+            addresses=anketa.reg_address,
+            person_id=person_id,
+            user_id=current_user.id,
+        ),
+        Contacts(
+            view="Телефон",
+            contact=anketa.contact_phone,
+            person_id=person_id,
+            user_id=current_user.id,
+        ),
+        Contacts(
+            view="Электронная почта",
+            contact=anketa.email,
+            person_id=person_id,
+            user_id=current_user.id,
+        ),
+    ]
+    items.extend(
+        [
+            Educations(
+                view=edu.education_type,
+                institution=edu.institution_name,
+                finished=edu.end_year,
+                specialty=edu.specialty,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for edu in anketa.education
+        ]
+        + [
+            Workplaces(
+                starts=work.begin_date,
+                finished=work.end_date,
+                now_work=work.current_job,
+                workplace=work.name,
+                addresses=work.address,
+                reason=work.fire_reason,
+                position=work.position,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for work in anketa.experience
+        ]
+        + [
+            Previous(
+                firstname=prev.first_name,
+                surname=prev.last_name,
+                patronymic=prev.mid_name,
+                changed=prev.year_change,
+                reason=prev.reason,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for prev in anketa.name_was_changed
+        ]
+        + [
+            Affilations(
+                view="Участвует в деятельности коммерческих организаций",
+                organization=aff.name,
+                inn=aff.inn,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for aff in anketa.organizations
+        ]
+        + [
+            Affilations(
+                view="Являлся государственным должностным лицом",
+                organization=aff.name,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for aff in anketa.state_organizations
+        ]
+        + [
+            Affilations(
+                view="Связанные лица работают в государственных организациях",
+                organization=aff.name,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for aff in anketa.related_organizations
+        ]
+        + [
+            Affilations(
+                view="Являлся государственным или муниципальным служащим",
+                organization=aff.name,
+                person_id=person_id,
+                user_id=current_user.id,
+            )
+            for aff in anketa.public_organizations
+        ],
+    )
+    return items
