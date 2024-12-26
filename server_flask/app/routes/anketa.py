360b461c0eb61d6e92246f9fdf38de202651338c
waldesem <105578047+waldesem@users.noreply.github.com>
2024-12-26 09:12:10 +0300

    Update anketa.py

--- server_flask/app/routes/anketa.py
+++ server_flask/app/routes/anketa.py
@@ -125,8 +125,10 @@ def change_self_id(person_id: int) -> Response:
         The HTTP status code is 200.
 
     """
-    db_session.execute(text(
-        "UPDATE persons SET editable = NOT editable, user_id = :user_id WHERE person_id = :person_id",
+    db_session.execute(
+        text(
+            "UPDATE persons SET editable = NOT editable, user_id = :user_id WHERE id = :person_id",
+        ),
         {
             "user_id": current_user.id,
             "person_id": person_id,
