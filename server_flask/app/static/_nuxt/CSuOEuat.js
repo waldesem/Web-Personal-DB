d49c5907abf4d232fb880f1040876ece78ce7683
Vladimir Semenenko <wsemenenko@gmail.com>
2025-03-29 14:06:09 +0300

    Update build

---
+++ server_flask/app/static/_nuxt/CSuOEuat.js
@@ -0,0 +1,1 @@
+import{d as t,p as o,w as s,c as a,n as r,o as p}from"./BkJrKHlt.js";const m=t({__name:"index",async setup(_){let e,n;return o("/persons"),[e,n]=s(()=>r("/persons")),await e,n(),(c,i)=>(p(),a("div",null,"Index"))}});export{m as default};
