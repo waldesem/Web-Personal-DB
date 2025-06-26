7f7fd683db66960f334d1cb97fab700304da8f6a
Vladimir <wsemenenko@gmail.com>
2025-06-26 09:15:26 +0300

    Fix issue

--- server_flask/app/utils/caching.py
+++ server_flask/app/utils/caching.py
@@ -6,26 +6,27 @@ from __future__ import annotations
 class DictCache[T]:
     """Dict Cache."""
 
-    def __init__(self, max_size: int = None) -> None:
+    def __init__(self, max_size: int | None = None) -> None:
         """Init cache."""
         self.max_size = max_size
-        self.data = {}
+        self._data = {}
 
     def get(self, key: str) -> T | None:
         """Get cache key."""
-        return self.data.get(key)
+        return self._data.get(key)
 
     def set(self, key: str, value: T) -> None:
         """Set cache value."""
-        self.data[key] = value
-        if max_size and len(self.data) > max_size:
-            del next(iter(self.data))
+        self._data[key] = value
+        if self.max_size and len(self._data) > self.max_size:
+            key = next(iter(self._data))
+            self.delete(key)
 
     def delete(self, key: str) -> None:
         """Delete cache key."""
-        if self.data.get(key):
-            del self.data[key]
+        if self._data.get(key):
+            del self._data[key]
 
     def clear(self) -> None:
         """Clear cache."""
-        self.data.clear()
+        self._data.clear()
