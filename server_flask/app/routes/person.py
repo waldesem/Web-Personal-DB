cfbc79cf03b4f283748d6e4ceaa3b2bd005f9640
waldesem <105578047+waldesem@users.noreply.github.com>
2025-08-01 17:16:49 +0300

    Update person.py

--- server_flask/app/routes/person.py
+++ server_flask/app/routes/person.py
@@ -33,7 +33,7 @@ def post_person(json_data: PersonIn) -> tuple[dict, int]:
     """Replace a record in persons table."""
     # Загружаем резюме, получаем id кандидата, а также был ли он ранее загружен
     cand_id, existed = upload_resume(json_data)
-    caching.set_data(cand_id, "")
+    caching.set_data(cand_id)
     return {"person_id": cand_id, "exists": existed}, 201
 
 
