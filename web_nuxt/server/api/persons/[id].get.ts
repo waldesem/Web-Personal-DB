5f775a47778ee01d652ee4bbe8bb02f733b82aa3
Vladimir Semenenko <wsemenenko@gmail.com>
2025-09-07 15:07:47 +0300

    Remove code to new branch

--- web_nuxt/server/api/persons/[id].get.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const db = useDatabase();
-
-  const itemId = getRouterParam(event, "id");
-  const item = getRouterParam(event, "item");
-
-  const stmt = db.prepare(`SELECT * FROM ${item} WHERE id = ?`);
-  return await stmt.get(itemId);
-});
