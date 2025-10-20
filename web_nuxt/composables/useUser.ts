6a563f58a9eb64a39a7ee8679ab1b7b9bea1044f
Vladimir <wsemenenko@gmail.com>
2025-10-20 12:32:00 +0300

    Update build

--- web_nuxt/composables/useUser.ts
+++ web_nuxt/composables/useUser.ts
@@ -3,18 +3,20 @@ import { useJwt } from "@vueuse/integrations/useJwt";
 
 // Создаем стейт для хранения данных пользователя
 export const useStateUser = () =>
-  useState("user", () => {
-    try {
-      const token = useCookie("token");
-      if (token.value) {
-        return useJwt(token.value).payload.value as Token;
-      } else {
+  useState("user", () =>
+    computed(() => {
+      try {
+        const token = useCookie("token");
+        if (token.value) {
+          return useJwt(token.value).payload.value as Token;
+        } else {
+          return {} as Token;
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error);
+        }
         return {} as Token;
       }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error(error);
-      }
-      return {} as Token;
-    }
-  });
+    })
+  );
