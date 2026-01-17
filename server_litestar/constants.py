8988e8a3e4d076c20f5533c0a67045b9aa4b8f44
Vladimir Semenenko <wsemenenko@gmail.com>
2026-01-17 17:44:26 +0300

    Fix issues

--- server_litestar/constants.py
+++ server_litestar/constants.py
@@ -3,7 +3,6 @@
 import os
 import secrets
 
-# from pathlib import Path  # noqa: ERA001
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,5 +15,5 @@ ACCESS_SECRET_KEY_LIVE = 60  # minutes
 REFRESH_SECRET_KEY_LIVE = 30 * 24 * 60  # minutes
 BASE_PATH = os.getenv("BASE_PATH")
 DEFAULT_PASSWORD = os.getenv("DEFAULT_PASSWORD")
+DATABASE_URI = f"postgresql+asyncpg://{os.getenv('USER')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}"
 # DATABASE_URI = "sqlite+aiosqlite:///" + str(Path(BASE_PATH, "database.db"))  # noqa: E501, ERA001
-DATABASE_URI = os.getenv("PG_DATABASE")
