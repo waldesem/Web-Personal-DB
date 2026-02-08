56d0dfcb1d277321c26fc0f064b8b3cb801a6907
Vladimir Semenenko <wsemenenko@gmail.com>
2026-02-08 09:19:16 +0300

    Fix issues

--- server_litestar/app/__init__.py
+++ server_litestar/app/__init__.py
@@ -1,6 +1,8 @@
 """Initialize the Flask application."""
 
 from litestar import Litestar, get
+from litestar.config.compression import CompressionConfig
+from litestar.config.cors import CORSConfig
 from litestar.exceptions import HTTPException
 from litestar.logging import LoggingConfig
 
@@ -8,6 +10,7 @@ from app.controllers import base_router
 from app.depends.auth import jwt_auth
 from app.tables.tables import alchemy_plugin
 
+cors_config = CORSConfig()
 logging_config = LoggingConfig(
     disable_stack_trace={404, ValueError},
 )
@@ -20,6 +23,8 @@ async def index() -> None:
 
 
 app = Litestar(
+    compression_config=CompressionConfig(backend="gzip", gzip_compress_level=6),
+    cors_config=cors_config,
     on_app_init=[jwt_auth.on_app_init],
     route_handlers=[index, base_router],
     logging_config=logging_config,
