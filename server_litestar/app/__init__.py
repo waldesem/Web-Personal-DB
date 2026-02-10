8b0102e69ad7682bbee5a80da5e58c8447a49d5d
Vladimir <wsemenenko@gmail.com>
2026-02-10 15:52:28 +0300

    Add app conf

--- server_litestar/app/__init__.py
+++ server_litestar/app/__init__.py
@@ -10,12 +10,6 @@ from app.controllers import base_router, index
 from app.depends.auth import jwt_auth
 from app.tables.tables import alchemy_plugin
 
-cors_config = CORSConfig()
-logging_config = LoggingConfig(
-    disable_stack_trace={404, ValueError},
-)
-
-
 app = Litestar(
     on_app_init=[jwt_auth.on_app_init],
     route_handlers=[
@@ -24,13 +18,25 @@ app = Litestar(
         create_static_files_router(
             path="/static",
             directories=["static"],
-            html_mode=True,
+            send_as_attachment=True,
         ),
     ],
-    compression_config=CompressionConfig(backend="gzip", gzip_compress_level=6),
-    cors_config=cors_config,
-    logging_config=logging_config,
+    compression_config=CompressionConfig(
+        backend="gzip",
+        minimum_size=1000,
+        gzip_compress_level=6,
+    ),
+    cors_config=CORSConfig(),
+    logging_config=LoggingConfig(
+        disable_stack_trace={404, ValueError},
+        formatters={
+            "standard": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            },
+        },
+        log_exceptions="always",
+        root={"level": "INFO", "handlers": ["queue_listener"]},
+    ),
     openapi_config=None,
     plugins=[alchemy_plugin],
-    debug=True,
 )
