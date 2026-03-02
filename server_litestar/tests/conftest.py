595fba72c7287c850665789448a08a3a17bd237e
Vladimir <wsemenenko@gmail.com>
2026-03-02 17:04:23 +0300

    Fix conftest

--- server_litestar/tests/conftest.py
+++ server_litestar/tests/conftest.py
@@ -16,34 +16,28 @@ app.debug = True
 app.openapi_config = None
 
 
-async def create_tokens(test_client: AsyncTestClient[Litestar]) -> Tokens | None:
-    """Create tokens for testing."""
-    response = await test_client.post(
-        "/routes/auth/login",
-        json={
-            "username": "",
-            "password": "",
-        },
-    )
-    resp = response.json()
-    if resp and resp.pop("message", None) == "success":
-        return Tokens(**resp)
-    return None
-
-
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture(scope="function")
 async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
     """Test client."""
     async with AsyncTestClient(app=app) as client:
         yield client
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture(scope="function")
 async def test_auth_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
     """Test client."""
     async with AsyncTestClient(app=app) as client:
-        if tokens := await create_tokens(client):
-            client.headers = {"Authorization": tokens.refresh_token}
+        response = await client.post(
+            "/routes/auth/login",
+            json={
+                "username": "",
+                "password": "",
+            },
+        )
+        resp = response.json()
+        if resp.pop("message", None) == "success":
+            tokens = Tokens(**resp)
+            client.headers = {"Authorization": tokens.access_token}
             yield client
         else:
             raise InternalServerException
