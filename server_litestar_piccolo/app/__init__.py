927b1e89add07bb45de1c3509668ffdb917a265d
Vladimir <wsemenenko@gmail.com>
2026-03-16 15:41:45 +0300

    Fix db pool

--- server_litestar_piccolo/app/__init__.py
+++ server_litestar_piccolo/app/__init__.py
@@ -1,5 +1,8 @@
 """Initialize the Litestar application."""
 
+from contextlib import asynccontextmanager
+from typing import TYPE_CHECKING
+
 from litestar import Litestar
 from litestar.config.compression import CompressionConfig
 from litestar.config.cors import CORSConfig
@@ -7,9 +10,13 @@ from litestar.logging import LoggingConfig
 from litestar.middleware.logging import LoggingMiddlewareConfig
 from litestar.openapi import OpenAPIConfig
 from litestar.static_files import create_static_files_router
+from piccolo.engine import engine_finder
 
 from app.controllers import base_router
 from app.middleware.auth import jwt_access
+
+if TYPE_CHECKING:
+    from types import AsyncGeneratorType
 
 route_handlers = [
     base_router,
@@ -39,11 +46,22 @@ logging_config = LoggingConfig(
 logging_middleware_config = LoggingMiddlewareConfig()
 
 
+@asynccontextmanager
+async def lifespan(_: Litestar) -> AsyncGeneratorType:
+    """Use a connection pool."""
+    engine = engine_finder()
+    assert engine
+    await engine.start_connection_pool()
+    yield
+    await engine.close_connection_pool()
+
+
 app = Litestar(
     on_app_init=[jwt_access.on_app_init],
     route_handlers=route_handlers,
     compression_config=compression_config,
     cors_config=CORSConfig(),
+    lifespan=[lifespan],
     logging_config=logging_config,
     middleware=[logging_middleware_config.middleware],
     openapi_config=OpenAPIConfig(title="STAFFSEC API", version="1.0.0"),
