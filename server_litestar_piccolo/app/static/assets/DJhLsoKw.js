1b21bb66fa790f921c6ece600677359b6ac0a418
Vladimir Semenenko <wsemenenko@gmail.com>
2026-03-28 15:37:12 +0300

    Update build

---
+++ server_litestar_piccolo/app/static/assets/DJhLsoKw.js
@@ -0,0 +1,1 @@
+import{l as e,bI as r,be as t}from"./yFN41sTO.js";function n(o){return e(()=>r(o)?!!t(o)?.closest("form"):!0)}export{n as u};
---
+++ server_litestar_piccolo/app/static/assets/DaM8lTg9.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./1MM_1JSD.js";import{d as o,o as a,c as r,w as s,R as _}from"./yFN41sTO.js";const d=o({__name:"index",setup(c){return(m,e)=>{const t=n;return a(),r(t,null,{default:s(()=>[...e[0]||(e[0]=[_("Index",-1)])]),_:1})}}});export{d as default};
